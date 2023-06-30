5b34693d4ce2c1bd5bb02352b35aa79d5fc079b9
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-07-01 01:04:09 +0700

    t

--- BinarySearchTree_159/BinarySearchTree_159.cpp
+++ BinarySearchTree_159/BinarySearchTree_159.cpp
@@ -114,3 +114,18 @@ public:
 		}
 	}
 };
+
+int main()
+{
+	BinaryTree obj;
+	while (true)
+	{
+		cout << "\nMenu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. Perform pereorder traversal" << endl;
+		cout << "4. perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+	}
+}
