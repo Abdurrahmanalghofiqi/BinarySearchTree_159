5c29371a322d58f1b48566743b60ce2b44d8c631
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-06-30 22:15:45 +0700

    m

--- BinarySearchTree_159/BinarySearchTree_159.cpp
+++ BinarySearchTree_159/BinarySearchTree_159.cpp
@@ -29,4 +29,14 @@ public:
 	}
 
 	void insert(string element)	//Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL);
+		newNode->info = element;	//Assign value to the data fiald of the new node
+		newNode->leftchild = NULL;	//Make the left child of the new node point to NULL
+		newNode->rightchild = NULL;	//Make the right child of the new node point to NULL
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode);	//Locate the node which will bi the parent of the node to be in serted
+	}
 };
